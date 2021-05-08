46849ac3638f2c63c035f812487df68b9e8f4f67
siavasham <66677857+siavasham@users.noreply.github.com>
2021-05-09 03:47:42 +0430

    ok

--- app/Http/Controllers/BankController.php
+++ app/Http/Controllers/BankController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller; 
 use App\Models\Bank; 
+use Validator;
 
 class BankController extends Controller
 {
--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller; 
 use App\Models\Message; 
+use Validator;
 
 class MessageController extends Controller
 {
